15b2dd1993265b607e413a6f431fc96df929d2ee
Boris <36640907+B0RlS@users.noreply.github.com>
2019-02-07 16:03:17 +0300

    Lesson 2 ( A )
    Lesson 2 ( A )

--- src/by/it/group673602/tsarikov/lesson02/A_VideoRegistrator.java
+++ src/by/it/group673602/tsarikov/lesson02/A_VideoRegistrator.java
@@ -1,15 +1,8 @@
 package by.it.group673602.tsarikov.lesson02;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-/*
-даны события events
-реализуйте метод calcStartTimes, так, чтобы число включений регистратора на
-заданный период времени (1) было минимальным, а все события events
-были зарегистрированы.
-
-Алгоритм жадный. Для реализации обдумайте надежный шаг.
-*/
 
 public class A_VideoRegistrator {
 
@@ -21,25 +14,24 @@ public class A_VideoRegistrator {
     }
     //модификаторы доступа опущены для возможности тестирования
     List<Double> calcStartTimes(double[] events, double workDuration){
-        //events - события которые нужно зарегистрировать
-        //timeWorkDuration время работы видеокамеры после старта
         List<Double> result;
         result = new ArrayList<>();
-        int i=0;                              //i - это индекс события events[i]
-        //комментарии от проверочного решения сохранены для подсказки, но вы можете их удалить.
-                                              //подготовка к жадному поглощению массива событий
-                                              //hint: сортировка Arrays.sort обеспечит скорость алгоритма
-                                              //C*(n log n) + C1*n = O(n log n)
-
-                                              //пока есть незарегистрированные события
-                                                //получим одно событие по левому краю
-                                                //и запомним время старта видеокамеры
-                                                //вычислим момент окончания работы видеокамеры
-                                                //и теперь пропустим все покрываемые события
-                                                //за время до конца работы, увеличивая индекс
-
-
-
+        int i=0;
+        List<Double> events2 = new ArrayList<>();
+        for(double ev : events){
+            events2.add(ev);
+        }
+        Collections.sort(events2);
+        double timeWorkDuration  = events2.get(0);
+        result.add(timeWorkDuration );
+        timeWorkDuration +=workDuration;
+        for (double time : events2) {
+            if (time > timeWorkDuration ) {
+                result.add(time);
+                timeWorkDuration  = time;
+                timeWorkDuration +=workDuration;
+            }
+        }
         return result;                        //вернем итог
     }
 }
