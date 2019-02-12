944030f673f7cb7ef91ca213b1c6a73f77f41a0f
Anthony <frostanton09@gmail.com>
2019-02-12 15:54:55 +0300

    Merge branch 'master' of https://github.com/Khmelov/PISL2019-01-29
    # Conflicts:
    #	src/by/it/a_khmelev/lesson01/FiboA.java

--- src/by/it/a_khmelev/lesson01/FiboA.java
+++ src/by/it/a_khmelev/lesson01/FiboA.java
@@ -30,14 +30,14 @@ public class FiboA {
         if (n==0){
             return 0;
         }
-       if (n==1)
-       {
-           return 1;
-       }
-       else
-       {
-           return calc(n-1)+calc(n-2);
-       }
+        if (n==1)
+        {
+            return 1;
+        }
+        else
+        {
+            return calc(n-1)+calc(n-2);
+        }
     }
 
 
@@ -63,4 +63,3 @@ public class FiboA {
 
 
 }
-
