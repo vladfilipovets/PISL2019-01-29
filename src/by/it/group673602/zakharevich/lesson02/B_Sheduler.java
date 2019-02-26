eb5b3c3b37a947d08dc627ffd1f62f08bbc3cefd
Владислав Захаревич <vl04ad99@gmail.com>
2019-02-26 11:49:17 +0300

    Lesson 2 A, B

--- src/by/it/group673602/zakharevich/lesson02/B_Sheduler.java
+++ src/by/it/group673602/zakharevich/lesson02/B_Sheduler.java
@@ -1,6 +1,7 @@
 package by.it.group673602.zakharevich.lesson02;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 /*
 даны интервальные события events
@@ -31,12 +32,12 @@ public class B_Sheduler {
     public static void main(String[] args) {
         B_Sheduler instance = new B_Sheduler();
         Event[] events = {  new Event(0, 3),  new Event(0, 1), new Event(1, 2), new Event(3, 5),
-                            new Event(1, 3),  new Event(1, 3), new Event(1, 3), new Event(3, 6),
-                            new Event(2, 7),  new Event(2, 3), new Event(2, 7), new Event(7, 9),
-                            new Event(3, 5),  new Event(2, 4), new Event(2, 3), new Event(3, 7),
-                            new Event(4, 5),  new Event(6, 7), new Event(6, 9), new Event(7, 9),
-                            new Event(8, 9),  new Event(4, 6), new Event(8, 10), new Event(7, 10)
-                          };
+                new Event(1, 3),  new Event(1, 3), new Event(1, 3), new Event(3, 6),
+                new Event(2, 7),  new Event(2, 3), new Event(2, 7), new Event(7, 9),
+                new Event(3, 5),  new Event(2, 4), new Event(2, 3), new Event(3, 7),
+                new Event(4, 5),  new Event(6, 7), new Event(6, 9), new Event(7, 9),
+                new Event(8, 9),  new Event(4, 6), new Event(8, 10), new Event(7, 10)
+        };
 
         List<Event> starts = instance.calcStartTimes(events,0,10);  //рассчитаем оптимальное заполнение аудитории
         System.out.println(starts);                                 //покажем рассчитанный график занятий
@@ -50,7 +51,15 @@ public class B_Sheduler {
         List<Event> result;
         result = new ArrayList<>();
         //ваше решение.
-
+        Arrays.sort(events, (o1, o2)->{
+            return Integer.compare(o1.stop, o2.stop);
+        });
+        for(Event event:events){
+            if(event.start >= from){
+                result.add(event);
+                from = event.stop;
+            }
+        }
 
 
 
