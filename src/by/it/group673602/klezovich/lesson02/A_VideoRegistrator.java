8710ca211746cf29a1f5fae666938829953f0b15
nastyaklezovich <nastyaklezovich@gmail.com>
2019-03-05 11:27:18 +0300

    Lesson2 B

--- src/by/it/group673602/klezovich/lesson02/A_VideoRegistrator.java
+++ src/by/it/group673602/klezovich/lesson02/A_VideoRegistrator.java
@@ -16,36 +16,32 @@ public class A_VideoRegistrator {
     public static void main(String[] args) {
         A_VideoRegistrator instance = new A_VideoRegistrator();
         double[] events = new double[]{1, 1.1, 1.6, 2.2, 2.4, 2.7, 3.9, 8.1, 9.1, 5.5, 3.7};
-        List<Double> starts = instance.calcStartTimes(events, 1); //рассчитаем моменты старта, с длинной сеанса 1
-        System.out.println(starts);                            //покажем моменты старта
+        List<Double> starts = instance.calcStartTimes(events, 1);
+        System.out.println(starts);
     }
 
-    //модификаторы доступа опущены для возможности тестирования
     List<Double> calcStartTimes(double[] events, double workDuration) {
-        //events - события которые нужно зарегистрировать
-        //timeWorkDuration время работы видеокамеры после старта
-        List<Double> result;
-        result = new ArrayList<>();
-        int i = 0;                              //i - это индекс события events[i]
-        //комментарии от проверочного решения сохранены для подсказки, но вы можете их удалить.
-        //подготовка к жадному поглощению массива событий
-        Arrays.sort(events);//hint: сортировка Arrays.sort обеспечит скорость алгоритма
-        //C*(n log n) + C1*n = O(n log n)
 
-        //пока есть незарегистрированные события
-        //получим одно событие по левому краю
-        //и запомним время старта видеокамеры
-        //вычислим момент окончания работы видеокамеры
+        List<Double> result = new ArrayList<>();
+
+        int i = 0;
+
+        Arrays.sort(events);
+
         while (i < events.length) {
+
             double start = events[i];
             result.add(start);
+
             i++;
-            //и теперь пропустим все покрываемые события
-            //за время до конца работы, увеличивая индекс
+            //пропуск событий, которые попадают в интервал времени workDuration
             while (i < events.length && events[i] <= start + workDuration) {
+
                 i++;
+
             }
         }
-        return result;                        //вернем итог
+        //моменты времени включения регистратора
+        return result;
     }
 }
--- src/by/it/group673602/klezovich/lesson02/B_Sheduler.java
+++ src/by/it/group673602/klezovich/lesson02/B_Sheduler.java
@@ -1,6 +1,7 @@
 package by.it.group673602.klezovich.lesson02;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 /*
 даны интервальные события events
@@ -11,7 +12,7 @@ import java.util.List;
 
 public class B_Sheduler {
     //событие у аудитории(два поля: начало и конец)
-    static class Event {
+    static class Event implements Comparable<Event>{
         int start;
         int stop;
 
@@ -22,21 +23,32 @@ public class B_Sheduler {
 
         @Override
         public String toString() {
-            return "("+ start +":" + stop + ")";
+            return "(" + start + ":" + stop + ")";
         }
+
+        @Override
+        public int compareTo(Event o) {
+            if (this.stop > o.stop)
+                return 1;
+            if (this.stop == o.stop)
+                return 0;
+            return -1;
+        }
+
     }
+
 
     public static void main(String[] args) {
         B_Sheduler instance = new B_Sheduler();
-        Event[] events = {  new Event(0, 3),  new Event(0, 1), new Event(1, 2), new Event(3, 5),
-                new Event(1, 3),  new Event(1, 3), new Event(1, 3), new Event(3, 6),
-                new Event(2, 7),  new Event(2, 3), new Event(2, 7), new Event(7, 9),
-                new Event(3, 5),  new Event(2, 4), new Event(2, 3), new Event(3, 7),
-                new Event(4, 5),  new Event(6, 7), new Event(6, 9), new Event(7, 9),
-                new Event(8, 9),  new Event(4, 6), new Event(8, 10), new Event(7, 10)
+        Event[] events = {new Event(0, 3), new Event(0, 1), new Event(1, 2), new Event(3, 5),
+                new Event(1, 3), new Event(1, 3), new Event(1, 3), new Event(3, 6),
+                new Event(2, 7), new Event(2, 3), new Event(2, 7), new Event(7, 9),
+                new Event(3, 5), new Event(2, 4), new Event(2, 3), new Event(3, 7),
+                new Event(4, 5), new Event(6, 7), new Event(6, 9), new Event(7, 9),
+                new Event(8, 9), new Event(4, 6), new Event(8, 10), new Event(7, 10)
         };
 
-        List<Event> starts = instance.calcStartTimes(events,0,10);  //рассчитаем оптимальное заполнение аудитории
+        List<Event> starts = instance.calcStartTimes(events, 0, 10);  //рассчитаем оптимальное заполнение аудитории
         System.out.println(starts);                                 //покажем рассчитанный график занятий
     }
 
@@ -48,12 +60,14 @@ public class B_Sheduler {
         List<Event> result;
         result = new ArrayList<>();
         //ваше решение.
+        Arrays.sort(events);
 
-
-
-
-
-
+        for (Event event : events) {
+            if (Integer.compare(event.start, from) >= 0) {
+                result.add(event);
+                from = event.stop;
+            }
+        }
         return result;                        //вернем итог
     }
 }
